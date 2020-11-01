5c8a4a79103f54c1cf77f2ce604cd731a035fe9e
dzaikov <diman2000gzz@gmail.com>
2020-11-02 02:17:54 +0700

    *#4 прием данных с сервера работает

--- Sandbags/app/src/main/java/ru/nsu/fit/sandbags/UpdateManager.java
+++ Sandbags/app/src/main/java/ru/nsu/fit/sandbags/UpdateManager.java
@@ -4,24 +4,16 @@ import android.graphics.PointF;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
+import ru.nsu.fit.sandbags.api.ServerAPI;
 import ru.nsu.fit.sandbags.map.PinStruct;
 
 public class UpdateManager {
+    private final ServerAPI serverAPI = new ServerAPI();
     private final List<List<PinStruct>> numbersOfSeats = new ArrayList<>();
     private final Object monitor = new Object();
-    private final Thread updaterThread = new Thread(()->{
-        while (!Thread.currentThread().isInterrupted()) {
-            synchronized (monitor) {
-                try {
-                    monitor.wait();
-                } catch (InterruptedException e) {
-                    break;
-                }
-            }
-            //Updater code
-        }
-    });
 
     public UpdateManager() {
         List<PinStruct> floorSeats = new ArrayList<>();
@@ -31,6 +23,32 @@ public class UpdateManager {
         }
         numbersOfSeats.set(3, floorSeats);
 
+        Thread updaterThread = new Thread(() -> {
+            List<List<Integer>> list = null;
+            while (!Thread.currentThread().isInterrupted()) {
+                synchronized (monitor) {
+                    try {
+                        monitor.wait();
+                        FutureTask<List<List<Integer>>> futureTask = serverAPI.getCurrentSandbagsState();
+                        list = futureTask.get();
+                    } catch (InterruptedException e) {
+                        break;
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //System.out.println(list.size());
+                if (list != null) {
+                    for (int i = 0; i < numbersOfSeats.size(); i++) {
+                        System.out.println(list.get(i).size());
+                        System.out.println(numbersOfSeats.get(i).size());
+                        for (int j = 0; j < numbersOfSeats.get(i).size(); j++) {
+                            numbersOfSeats.get(i).get(j).setNum(list.get(i).get(j));
+                        }
+                    }
+                }
+            }
+        });
         updaterThread.start();
     }
 
--- Sandbags/app/src/main/java/ru/nsu/fit/sandbags/api/ServerAPI.java
+++ Sandbags/app/src/main/java/ru/nsu/fit/sandbags/api/ServerAPI.java
@@ -1,50 +1,45 @@
 package ru.nsu.fit.sandbags.api;
 
 import android.util.JsonReader;
-
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class ServerAPI {
-    public Future<List<List<Integer>>> getCurrentSandbagsState() {
-        Callable task = () -> {
+    public FutureTask<List<List<Integer>>> getCurrentSandbagsState() {
+        Callable<List<List<Integer>>> task = () -> {
             List<List<Integer>> currentState = new ArrayList<>();
             try {
-                URL serverEndpoint = new URL("http://127.0.0.1:8000/api/v1/state/");
-                HttpURLConnection myConnection =
-                        (HttpURLConnection) serverEndpoint.openConnection();
+                URL serverEndpoint = new URL("https://sandbags-project.herokuapp.com/api/v1/state/");
+                HttpsURLConnection myConnection =
+                        (HttpsURLConnection) serverEndpoint.openConnection();
                 myConnection.setRequestProperty("Accept", "application/json");
                 if (myConnection.getResponseCode() == 200) {
                     Reader responseBodyReader =
-                            new InputStreamReader(myConnection.getInputStream(),
-                                    "UTF-8");
-                    Scanner scanner = new Scanner(responseBodyReader);
-                    while (scanner.hasNext())
-                        System.out.println(scanner.next());
+                            new InputStreamReader(myConnection.getInputStream(), "UTF-8");
                     JsonReader jsonReader = new JsonReader(responseBodyReader);
                     jsonReader.beginArray(); //floors
                     while (jsonReader.hasNext()) {
                         jsonReader.beginObject(); // floor
                         String key = jsonReader.nextName();
+                        System.out.println(key);
                         if (key.equals("number")) { //floor number
                             jsonReader.skipValue();
                             key = jsonReader.nextName();
                             if (key.equals("places")) {
                                 jsonReader.beginArray(); //places
                                 currentState.add(parsePlaces(jsonReader));
-                                System.out.println(currentState.size());
                                 jsonReader.endArray();
                             }
                         }
@@ -63,8 +58,8 @@ public class ServerAPI {
         };
 
         FutureTask<List<List<Integer>>> future = new FutureTask<>(task);
-        Thread thread = new Thread(future);
-        thread.start();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(future);
         return future;
     }
 
